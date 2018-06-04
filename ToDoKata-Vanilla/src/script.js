1a1c2a3cd140d3369538330f3b5c385067dde214
Clemens Stift <clemens@stift.online>
2018-06-04 13:26:37 +0200

    Implemented logic for refreshing ToDoList and added dummy data.

--- ToDoKata-Vanilla/src/script.js
+++ ToDoKata-Vanilla/src/script.js
@@ -1,36 +1,110 @@
 window.addEventListener("load", init, false);
 
-var databaseService = {
-    requestData : function(){
+// NO BACKEND PLANNED SO FAR.
+// var databaseService = {
+//     geData : function(){
 
-    }
-    , pushData : function(){
+//     }
+//     , pushData : function(){
 
-    }
-}
+//     }
+// }
 
 var viewHandler = {
     initEventListeners : function () {
-
+        document.getElementById("filterByToday").addEventListener("click", ()=>{
+            console.log("Filter by today");
+            ToDoModel.filterListOfToDosByDate();
+        });
+        document.getElementById("filterByWeek").addEventListener("click", ()=>{
+            console.log("Filter by this week");
+            ToDoModel.filterListOfToDosByDate();
+        });
+        document.getElementById("filterByMonth").addEventListener("click", ()=>{
+            console.log("Filter by this month");
+            ToDoModel.filterListOfToDosByDate();
+        });
+        document.getElementById("filterByBacklog").addEventListener("click", ()=>{
+            console.log("Filter by Backlog");
+        });
+        document.getElementById("filterByInProgress").addEventListener("click", ()=>{
+            console.log("Filter by In Progress");
+        });
+        document.getElementById("filterByDone").addEventListener("click", ()=>{
+            console.log("Filter by Done");
+        });
+        document.getElementById("addTodo").addEventListener("click", ()=>{
+            console.log("Pressed AddTodo");
+        });
     }
     , displayNewToDoDialogue : function() {
 
     }
-    , displayListOfToDos : function(lsOfToDos){
+    , refreshListOfToDos : function(lsOfToDos){
+        viewHandler.clearListOfTodos();
 
+        dnToDoListMainView = document.getElementById("mainView");
+
+        ToDoModel.lsToDos.forEach((ToDo)=>{
+            dnNewToDo = document.createElement("div");
+            dnNewToDo.classList += "todoCard";
+
+            dnNewToDoHeader = document.createElement("h3");
+            dnNewToDoHeader.innerHTML = ToDo.title;
+
+            dnNewToDo.appendChild(dnNewToDoHeader);
+
+            dnNewToDoBody = document.createElement("p");
+            dnNewToDoBody.innerHTML = ToDo.content;
+
+            dnNewToDo.appendChild(dnNewToDoBody);
+
+            dnNewToDoMetaBar = document.createElement("div");
+            dnNewToDoMetaBar.classList += "metaBar";
+
+                dnNewToDoMetaBarOwner = document.createElement("div");
+                dnNewToDoMetaBarOwner.classList += "owner inline-block";
+                dnNewToDoMetaBarOwner.innerHTML = ToDo.responsible;
+                dnNewToDoMetaBar.appendChild(dnNewToDoMetaBarOwner);
+
+                dnNewToDoMetaBarEstimate = document.createElement("div");
+                dnNewToDoMetaBarEstimate.classList += "estimate inline-block";
+                dnNewToDoMetaBarEstimate.innerHTML = ToDo.timeEstimate;
+                dnNewToDoMetaBar.appendChild(dnNewToDoMetaBarEstimate);
+
+                dnNewToDoMetaBarDueDate = document.createElement("div");
+                dnNewToDoMetaBarDueDate.classList += "dueDate inline-block";
+                dnNewToDoMetaBarDueDate.innerHTML = ToDo.dueDate.toLocaleDateString();
+                dnNewToDoMetaBar.appendChild(dnNewToDoMetaBarDueDate);
+
+                dnNewToDoDoneButton = document.createElement("button");
+                dnNewToDoDoneButton.classList += "done";
+                dnNewToDoDoneButton.innerHTML = "-> Done"
+                dnNewToDoMetaBar.appendChild(dnNewToDoDoneButton);
+
+            dnNewToDo.appendChild(dnNewToDoMetaBar);
+
+            dnToDoListMainView.appendChild(dnNewToDo);
+        });
     }
     , fetchToDoInputData : function(){
 
     }
-    , 
+    , clearListOfTodos : function(){
+        dnToDoListMainView = document.getElementById("mainView");
+        while(dnToDoListMainView.firstChild) {
+            dnToDoListMainView.removeChild(dnToDoListMainView.firstChild);
+        }
+    }
 
 }
 
 var ToDoModel = {
     lsToDos : []
-    , addToDo : function () {
-        newTodo = new ToDo();
-        lsToDos.push(newToDo);
+    , addToDo : function (strTitle, strContent, strResponsible, dueDate, timeEstimate) {
+        var newToDo = new ToDo(strTitle, strContent, strResponsible, dueDate, timeEstimate);
+        ToDoModel.lsToDos.push(newToDo);
+        viewHandler.refreshListOfToDos();
     }
     , filterListOfToDosByDate : function(date){
 
@@ -54,5 +128,11 @@ class ToDo {
 }
 
 function init(){
-    console.log("Loaded.");
+    ToDoModel.addToDo("1st Todo", "Working with plain JS can be fun.", "Clemens Stift", new Date(2018, 4, 28), 4);
+    ToDoModel.addToDo("2nd Todo", "Should you be motivated to figure out why all the frameworks exist.", "Marlon Alagoda", new Date(2018, 4, 30), 2);
+    ToDoModel.addToDo("3rd Todo", "Before switching to learn more about ReactJS", "Julian Tandler", new Date(2018, 5, 2), 2);
+
+    viewHandler.refreshListOfToDos();
+
+    viewHandler.initEventListeners();
 }
